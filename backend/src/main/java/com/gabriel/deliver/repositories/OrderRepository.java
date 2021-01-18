210e282b3c1f1c1aca7a10594db65e66d5c30eb5
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-01-17 23:21:35 -0300

    GET /orders

--- backend/src/main/java/com/gabriel/deliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/gabriel/deliver/repositories/OrderRepository.java
@@ -1,11 +1,17 @@
 package com.gabriel.deliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.gabriel.deliver.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> { // entity and data type of the primary id
 	
 	// this object accesses the database
+	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products " 
+			+ " WHERE obj.status = 0 ORDER BY obj.moment ASC")
+	List<Order> findOrdersWithProducts();
 	
 }
